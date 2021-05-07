c9c1da7e9641f6ff6e1ca67cc23e0eea2a4a1dec
cladams0203 <cladams0203@gmail.com>
2021-05-07 19:25:19 -0400

    typo

--- src/data/dbConfig.ts
+++ src/data/dbConfig.ts
@@ -4,9 +4,9 @@ import knexConfig from "../knexfile";
 
 const checkDebug = () => {
   if (process.env.DEBUG) {
-    return knexTinyLogger(Knex(knexConfig[process.env.DB_URL || "development"]));
+    return knexTinyLogger(Knex(knexConfig[process.env.DB_ENV || "development"]));
   } else {
-    return Knex(knexConfig[process.env.DB_URL || "development"]);
+    return Knex(knexConfig[process.env.DB_ENV || "development"]);
   }
 };
 export default checkDebug();
